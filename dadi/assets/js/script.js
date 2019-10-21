6899580bdd23cb1e3fad967068411ecdb4e56601
Francesco <franc.ns@gmail.com>
2019-10-21 16:35:05 +0200

    centralized output in the dices exercise

--- dadi/assets/js/script.js
+++ dadi/assets/js/script.js
@@ -1,21 +1,22 @@
 // Gioco dei dadi
 
 // creazione variabili
-var dice1, dice2;
+var dice1, dice2, result;
 
 // assegnazione valori random per tiro del dado
 dice1 = Math.floor(Math.random() * 6) + 1;
 dice2 = Math.floor(Math.random() * 6) + 1;
 
-// console log valore dadi
-console.log('DADO 1 = ', dice1);
-console.log('DADO 2 = ', dice2);
-
 // confronto tra i due dadi
 if (dice1 > dice2) {
-  console.log('Vince il dado 1!');
+  result = 'Vince il dado 1';
 } else if (dice2 > dice1) {
-  console.log('Vince il dado 2!');
+  result = 'Vince il dado 2';
 } else {
-  console.log('Pareggio!');
+  result = 'Pareggio!';
 }
+
+// output
+console.log('DADO 1 = ', dice1);
+console.log('DADO 2 = ', dice2);
+console.log(result);
