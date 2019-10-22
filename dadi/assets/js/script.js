3cb67bd352ae6090d31655705283ce546febe3d1
Francesco <franc.ns@gmail.com>
2019-10-22 15:09:26 +0200

    added a second dice to each player in the dices exercise

--- dadi/assets/js/script.js
+++ dadi/assets/js/script.js
@@ -1,22 +1,28 @@
 // Gioco dei dadi
 
 // creazione variabili
-var dice1, dice2, result;
+var dice1, dice2, dice3, dice4, player1, player2, result;
 
-// assegnazione valori random per tiro del dado
+// assegnazione valori random per tiri dei dadi
 dice1 = Math.floor(Math.random() * 6) + 1;
 dice2 = Math.floor(Math.random() * 6) + 1;
+dice3 = Math.floor(Math.random() * 6) + 1;
+dice4 = Math.floor(Math.random() * 6) + 1;
+
+// calcolo somma dei due dadi
+player1 = dice1 + dice2;
+player2 = dice3 + dice4;
 
 // confronto tra i due dadi
-if (dice1 > dice2) {
-  result = 'Vince il dado 1';
-} else if (dice2 > dice1) {
-  result = 'Vince il dado 2';
+if (player1 > player2) {
+  result = 'Vince il giocatore 1';
+} else if (player2 > player1) {
+  result = 'Vince il giocatore 2';
 } else {
   result = 'Pareggio!';
 }
 
 // output
-console.log('DADO 1 = ', dice1);
-console.log('DADO 2 = ', dice2);
+console.log('GIOCATORE 1: ', dice1, '+', dice2, '=', player1);
+console.log('GIOCATORE 2: ', dice3, '+', dice4, '=', player2);
 console.log(result);
